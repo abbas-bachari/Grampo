8c1411cc99838e5163889654b3c6423e36058e90
Abbas bachari <93954297+abbas-bachari@users.noreply.github.com>
2024-09-07 05:26:47 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Grampo',
-    version='1.0.0',
+    version='1.0.1',
     author='Abbas Bachari',
     author_email='abbas-bachari@hotmail.com',
     description='A Python Telegram API Library , with official Telegram APIs.',
